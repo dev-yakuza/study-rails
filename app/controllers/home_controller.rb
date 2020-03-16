06c6be82f39dc2a7652188229486789295cff6f9
dev-yakuza <dev.yakuza@gmail.com>
2020-03-16 18:31:36 +0900

    add creating data and read/show data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,21 @@ class HomeController < ApplicationController
         name = params[:name]
         @name = name ? name : 'dev-yakuza'
     end
+
+    def form
+
+    end
+
+    def create
+        post = Post.new
+        post.title = params[:title]
+        post.content = params[:content]
+        post.save
+
+        redirect_to '/list'
+    end
+
+    def list
+        @posts = Post.all
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get '/', to: 'home#index'
   post '/', to: 'home#index'
+
+  get '/form', to: 'home#form'
+  post '/create', to: 'home#create'
+  get '/list', to: 'home#list'
 end
